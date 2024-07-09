a07004a74bff4288d9a7807ebf172c4c96c700a2
nathanhollows <me@nathanhollows.com>
2024-07-09 16:30:07 +1200

    fix: db rel direction

--- models/instance_location_content.go
+++ models/instance_location_content.go
@@ -3,10 +3,9 @@ package models
 type InstanceLocationContent struct {
 	baseModel
 
-	ID                 string           `bun:",pk" json:"id"`
-	InstanceLocationID string           `bun:",notnull" json:"instance_location_id"`
-	Content            string           `bun:",null" json:"content"`
-	InstanceLocation   InstanceLocation `bun:"rel:has-one,join:instance_location_id=id" json:"instance_location"`
+	ID                 string `bun:",pk" json:"id"`
+	InstanceLocationID string `bun:",notnull" json:"instance_location_id"`
+	Content            string `bun:",null" json:"content"`
 }
 
 type InstanceLocationContents []InstanceLocationContent
--- models/instance_locations.go
+++ models/instance_locations.go
@@ -9,13 +9,14 @@ import (
 type InstanceLocation struct {
 	baseModel
 
-	ID         string             `bun:",pk" json:"id"`
-	InstanceID string             `bun:",notnull" json:"instance_id"`
-	LocationID string             `bun:",notnull" json:"location_id"`
-	CriteriaID string             `bun:",notnull" json:"criteria_id"`
-	Instance   Instance           `bun:"rel:has-one,join:instance_id=id" json:"instance"`
-	Location   Location           `bun:"rel:has-one,join:location_id=code" json:"location"`
-	Criteria   CompletionCriteria `bun:"rel:has-one,join:criteria_id=id" json:"criteria"`
+	ID         string                  `bun:",pk" json:"id"`
+	InstanceID string                  `bun:",notnull" json:"instance_id"`
+	LocationID string                  `bun:",notnull" json:"location_id"`
+	CriteriaID string                  `bun:",notnull" json:"criteria_id"`
+	Instance   Instance                `bun:"rel:has-one,join:instance_id=id" json:"instance"`
+	Location   Location                `bun:"rel:has-one,join:location_id=code" json:"location"`
+	Criteria   CompletionCriteria      `bun:"rel:has-one,join:criteria_id=id" json:"criteria"`
+	Content    InstanceLocationContent `bun:"rel:has-one,join:id=instance_location_id" json:"instance_location_content"`
 }
 
 type InstanceLocations []InstanceLocation
@@ -29,6 +30,8 @@ func FindAllInstanceLocations(ctx context.Context) (InstanceLocations, error) {
 		Model(&instanceLocations).
 		Where("instance_location.instance_id = ?", user.CurrentInstance.ID).
 		Relation("Location").
+		Relation("Criteria").
+		Relation("Content").
 		Scan(ctx)
 	if err != nil {
 		log.Error(err)
