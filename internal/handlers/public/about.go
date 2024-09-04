1ac2df8afce03e60ee19396e3da115ccfb0af4dd
nathanhollows <me@nathanhollows.com>
2024-09-04 22:41:02 +1200

    feat: about page

---
+++ internal/handlers/public/about.go
@@ -0,0 +1,16 @@
+package handlers
+
+import (
+	"net/http"
+
+	templates "github.com/nathanhollows/Rapua/internal/templates/public"
+)
+
+func (h *PublicHandler) About(w http.ResponseWriter, r *http.Request) {
+	c := templates.About()
+	err := templates.PublicLayout(c, "About").Render(r.Context(), w)
+	if err != nil {
+		h.Logger.Error("Error rendering index", "err", err)
+		return
+	}
+}
