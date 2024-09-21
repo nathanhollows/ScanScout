06054b4e7a505b3358685c47d734d0cfe78ee99e
nathanhollows <me@nathanhollows.com>
2024-09-21 16:13:01 +1200

    fix: boosted link, utc to local conversion

--- internal/templates/players/lobby_templ.go
+++ internal/templates/players/lobby_templ.go
@@ -157,7 +157,7 @@ func instructions(instance models.InstanceSettings) templ.Component {
 			var templ_7745c5c3_Var7 string
 			templ_7745c5c3_Var7, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprint(instance.MaxNextLocations))
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/players/lobby.templ`, Line: 142, Col: 73}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/players/lobby.templ`, Line: 146, Col: 73}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var7))
 			if templ_7745c5c3_Err != nil {
