53beb2739d0343fff84fbd00f75b6e06ece7b97b
nathanhollows <me@nathanhollows.com>
2024-10-10 22:26:21 +1300

    style: reordered functions to align with Block interface

--- internal/blocks/markdown_block.go
+++ internal/blocks/markdown_block.go
@@ -11,30 +11,26 @@ type MarkdownBlock struct {
 	Content string `json:"content"`
 }
 
-func (b *MarkdownBlock) RequiresValidation() bool { return false }
+// Basic Attributes Getters
 
-func (b *MarkdownBlock) ValidatePlayerInput(state *models.TeamBlockState, input map[string]string) error {
-	// No validation required
-	state.IsComplete = true
-	return nil
-}
-
-func (b *MarkdownBlock) GetName() string { return "Markdown" }
+func (b *MarkdownBlock) GetID() string         { return b.ID }
+func (b *MarkdownBlock) GetType() string       { return "markdown" }
+func (b *MarkdownBlock) GetLocationID() string { return b.LocationID }
+func (b *MarkdownBlock) GetName() string       { return "Markdown" }
 func (b *MarkdownBlock) GetDescription() string {
 	return "Text written in Markdown."
 }
+func (b *MarkdownBlock) GetOrder() int  { return b.Order }
+func (b *MarkdownBlock) GetPoints() int { return b.Points }
 func (b *MarkdownBlock) GetIconSVG() string {
-	return `<svg class="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 8L18 16" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M22 12L18 16L14 12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M2 16L2 8L6 12L10 8V16" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg> `
+	return `<svg class="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 8L18 16" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M22 12L18 16L14 12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M2 16L2 8L6 12L10 8V16" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>`
 }
-func (b *MarkdownBlock) GetType() string       { return "markdown" }
-func (b *MarkdownBlock) GetID() string         { return b.ID }
-func (b *MarkdownBlock) GetOrder() int         { return b.Order }
-func (b *MarkdownBlock) GetLocationID() string { return b.LocationID }
-func (b *MarkdownBlock) GetPoints() int        { return b.Points }
 func (b *MarkdownBlock) GetData() json.RawMessage {
 	data, _ := json.Marshal(b)
 	return data
 }
+
+// Data Operations
 
 func (b *MarkdownBlock) ParseData() error {
 	return json.Unmarshal(b.Data, b)
@@ -45,6 +41,19 @@ func (b *MarkdownBlock) UpdateBlockData(data map[string]string) error {
 	return nil
 }
 
+// Validation and Points Calculation
+
+func (b *MarkdownBlock) RequiresValidation() bool {
+	return false
+}
+
+func (b *MarkdownBlock) ValidatePlayerInput(state *models.TeamBlockState, input map[string]string) error {
+	// No validation required for MarkdownBlock; mark as complete
+	state.IsComplete = true
+	return nil
+}
+
 func (b *MarkdownBlock) CalculatePoints(input map[string]string) (int, error) {
+	// MarkdownBlock has no points to calculate
 	return 0, nil
 }
--- internal/blocks/password_block.go
+++ internal/blocks/password_block.go
@@ -19,50 +19,20 @@ type passwordBlockData struct {
 	Guesses  []string `json:"guesses"`
 }
 
-func (b *PasswordBlock) RequiresValidation() bool { return true }
+// Basic Attributes Getters
 
-func (b *PasswordBlock) ValidatePlayerInput(state *models.TeamBlockState, input map[string]string) error {
-	var err error
-	newPlayerData := passwordBlockData{}
-	if state.PlayerData != nil {
-		json.Unmarshal(state.PlayerData, &newPlayerData)
-	}
-
-	if newPlayerData.Attempts == 0 {
-		newPlayerData.Guesses = []string{}
-	}
-	newPlayerData.Attempts++
-	newPlayerData.Guesses = append(newPlayerData.Guesses, input["password"])
-
-	if input["password"] != b.Password {
-		state.PlayerData, err = json.Marshal(newPlayerData)
-		if err != nil {
-			return fmt.Errorf("Error saving player data")
-		}
-		return fmt.Errorf("Incorrect password")
-	}
-
-	state.PlayerData, err = json.Marshal(newPlayerData)
-	if err != nil {
-		return fmt.Errorf("Error saving player data")
-	}
-	state.IsComplete = true
-	state.PointsAwarded = b.Points
-	return nil
-}
-
-func (b *PasswordBlock) GetName() string { return "Password" }
+func (b *PasswordBlock) GetID() string         { return b.ID }
+func (b *PasswordBlock) GetType() string       { return "password" }
+func (b *PasswordBlock) GetLocationID() string { return b.LocationID }
+func (b *PasswordBlock) GetName() string       { return "Password" }
 func (b *PasswordBlock) GetDescription() string {
 	return "Players must enter the correct password."
 }
+func (b *PasswordBlock) GetOrder() int  { return b.Order }
+func (b *PasswordBlock) GetPoints() int { return b.Points }
 func (b *PasswordBlock) GetIconSVG() string {
 	return `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-key-round"><path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"/><circle cx="16.5" cy="7.5" r=".5" fill="currentColor"/></svg>`
 }
-func (b *PasswordBlock) GetType() string       { return "password" }
-func (b *PasswordBlock) GetID() string         { return b.ID }
-func (b *PasswordBlock) GetOrder() int         { return b.Order }
-func (b *PasswordBlock) GetLocationID() string { return b.LocationID }
-func (b *PasswordBlock) GetPoints() int        { return b.Points }
 func (b *PasswordBlock) GetAdminData() interface{} {
 	return &b
 }
@@ -70,6 +40,8 @@ func (b *PasswordBlock) GetData() json.RawMessage {
 	data, _ := json.Marshal(b)
 	return data
 }
+
+// Data Operations
 
 func (b *PasswordBlock) ParseData() error {
 	return json.Unmarshal(b.Data, b)
@@ -79,6 +51,40 @@ func (b *PasswordBlock) UpdateBlockData(data map[string]string) error {
 	b.Content = data["content"]
 	b.Password = data["block-passphrase"]
 	b.Fuzzy = data["fuzzy"] == "on"
+	return nil
+}
+
+// Validation and Points Calculation
+
+func (b *PasswordBlock) RequiresValidation() bool { return true }
+
+func (b *PasswordBlock) ValidatePlayerInput(state *models.TeamBlockState, input map[string]string) error {
+	var err error
+	newPlayerData := passwordBlockData{}
+	if state.PlayerData != nil {
+		json.Unmarshal(state.PlayerData, &newPlayerData)
+	}
+
+	// Increment the number of attempts and save guesses
+	newPlayerData.Attempts++
+	newPlayerData.Guesses = append(newPlayerData.Guesses, input["password"])
+
+	if input["password"] != b.Password {
+		// Incorrect password, save player data and return an error
+		state.PlayerData, err = json.Marshal(newPlayerData)
+		if err != nil {
+			return fmt.Errorf("Error saving player data")
+		}
+		return fmt.Errorf("Incorrect password")
+	}
+
+	// Correct password, update state to complete
+	state.PlayerData, err = json.Marshal(newPlayerData)
+	if err != nil {
+		return fmt.Errorf("Error saving player data")
+	}
+	state.IsComplete = true
+	state.PointsAwarded = b.Points
 	return nil
 }
 
