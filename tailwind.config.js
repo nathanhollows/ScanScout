a403a84ddbf4cd1d9654c42ef703fb903eef67ef
nathanhollows <me@nathanhollows.com>
2024-10-12 15:00:20 +1300

    style: new wobble animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,42 @@
 module.exports = {
   content: ["./web/templates/**/*.html", "./internal/templates/**/*.templ", "./internal/blocks/templates/**/*.templ"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wobble: {
+          from: {
+            transform: 'translate3d(0, 0, 0)',
+          },
+
+          '15%': {
+            transform: 'translate3d(-25%, 0, 0) rotate3d(0, 0, 1, -5deg)',
+          },
+
+          '30%': {
+            transform: 'translate3d(20%, 0, 0) rotate3d(0, 0, 1, 3deg)',
+          },
+
+          '45%': {
+            transform: 'translate3d(-15%, 0, 0) rotate3d(0, 0, 1, -3deg)',
+          },
+
+          '60%': {
+            transform: 'translate3d(10%, 0, 0) rotate3d(0, 0, 1, 2deg)',
+          },
+
+          '75%': {
+            transform: 'translate3d(-5%, 0, 0) rotate3d(0, 0, 1, -1deg)',
+          },
+
+          to: {
+            transform: 'translate3d(0, 0, 0)',
+          },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/container-queries'),
