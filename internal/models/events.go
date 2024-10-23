6300cf81f76c000e1a5c8d451400957dc89731ad
nathanhollows <me@nathanhollows.com>
2024-10-23 22:55:14 +1300

    refactor: logic doesn't belong in models

--- internal/models/events.go
+++ internal/models/events.go
@@ -1,11 +1,5 @@
 package models
 
-import (
-	"context"
-	"github.com/google/uuid"
-	"github.com/nathanhollows/Rapua/db"
-)
-
 type Event struct {
 	baseModel
 
@@ -18,32 +12,4 @@ type Event struct {
 
 	Instance Instance `bun:"rel:has-one,join:instance_id=id"`
 	Location Location `bun:"rel:has-one,join:location_id=id"`
-}
-
-// Save saves or updates an event
-func (e *Event) Save(ctx context.Context) error {
-	var err error
-	if e.ID == "" {
-		e.ID = uuid.New().String()
-		_, err = db.DB.NewInsert().Model(e).Exec(ctx)
-	} else {
-		_, err = db.DB.NewUpdate().Model(e).WherePK().Exec(ctx)
-	}
-	return err
-}
-
-// Delete removes the event from the database
-func (e *Event) Delete(ctx context.Context) error {
-	_, err := db.DB.NewDelete().Model(e).WherePK().Exec(ctx)
-	return err
-}
-
-// FindEventByID finds an event by its ID
-func FindEventByID(ctx context.Context, eventID string) (*Event, error) {
-	event := &Event{}
-	err := db.DB.NewSelect().Model(event).Where("id = ?", eventID).Scan(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return event, nil
 }
