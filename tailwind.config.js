cc688ce6bf5499d651b21eb8bd93fbd49239d130
nathanhollows <me@nathanhollows.com>
2024-10-31 17:17:37 +1300

    chore: non-functional automatic updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,33 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./web/templates/**/*.html", 
-    "./internal/templates/**/*.templ", 
-    "./internal/blocks/templates/**/*.templ"
-  ],
-  theme: {
-    extend: {
-      keyframes: {
-        wobble: {
-          from: { transform: 'translate3d(0, 0, 0)', },
-          '15%': { transform: 'translate3d(-25%, 0, 0) rotate3d(0, 0, 1, -5deg)', },
-          '30%': { transform: 'translate3d(20%, 0, 0) rotate3d(0, 0, 1, 3deg)', },
-          '45%': { transform: 'translate3d(-15%, 0, 0) rotate3d(0, 0, 1, -3deg)', },
-          '60%': { transform: 'translate3d(10%, 0, 0) rotate3d(0, 0, 1, 2deg)', },
-          '75%': { transform: 'translate3d(-5%, 0, 0) rotate3d(0, 0, 1, -1deg)', },
-          to: { transform: 'translate3d(0, 0, 0)', },
-        },
-      },
-      animation: {
-        wiggle: 'wiggle 1s ease-in-out infinite',
+export const content = [
+  "./web/templates/**/*.html",
+  "./internal/templates/**/*.templ",
+  "./internal/blocks/templates/**/*.templ"
+];
+export const theme = {
+  extend: {
+    keyframes: {
+      wobble: {
+        from: { transform: 'translate3d(0, 0, 0)', },
+        '15%': { transform: 'translate3d(-25%, 0, 0) rotate3d(0, 0, 1, -5deg)', },
+        '30%': { transform: 'translate3d(20%, 0, 0) rotate3d(0, 0, 1, 3deg)', },
+        '45%': { transform: 'translate3d(-15%, 0, 0) rotate3d(0, 0, 1, -3deg)', },
+        '60%': { transform: 'translate3d(10%, 0, 0) rotate3d(0, 0, 1, 2deg)', },
+        '75%': { transform: 'translate3d(-5%, 0, 0) rotate3d(0, 0, 1, -1deg)', },
+        to: { transform: 'translate3d(0, 0, 0)', },
       },
     },
+    animation: {
+      wiggle: 'wiggle 1s ease-in-out infinite',
+    },
   },
-  plugins: [
-    require('@tailwindcss/container-queries'),
-    require("@tailwindcss/typography"),
-    require("daisyui"),
-  ],
-  daisyui: {
-    themes: ["cupcake", "dracula"],
-    darkTheme: "dracula",
-  },
+};
+export const plugins = [
+  require('@tailwindcss/container-queries'),
+  require("@tailwindcss/typography"),
+  require("daisyui"),
+];
+export const daisyui = {
+  themes: ["cupcake", "dracula"],
+  darkTheme: "dracula",
 };
