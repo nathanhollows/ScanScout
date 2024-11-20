d3b406c2dcf29ec4f9a45ad92dae49c5ecb0296d
nathanhollows <me@nathanhollows.com>
2024-11-20 16:52:56 +1300

    fix: marker names not populating on activity overview

--- internal/templates/admin/activity_templ.go
+++ internal/templates/admin/activity_templ.go
@@ -568,9 +568,9 @@ func TeamActivity(settings models.InstanceSettings, team models.Team, notificati
 					return templ_7745c5c3_Err
 				}
 				var templ_7745c5c3_Var21 string
-				templ_7745c5c3_Var21, templ_7745c5c3_Err = templ.JoinStringErrs(location.Marker.Name)
+				templ_7745c5c3_Var21, templ_7745c5c3_Err = templ.JoinStringErrs(location.Name)
 				if templ_7745c5c3_Err != nil {
-					return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/admin/activity.templ`, Line: 465, Col: 29}
+					return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/admin/activity.templ`, Line: 465, Col: 22}
 				}
 				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var21))
 				if templ_7745c5c3_Err != nil {
