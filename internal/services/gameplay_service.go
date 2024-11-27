9647507d2e40fa77a32f5642da904fc15f685f50
nathanhollows <me@nathanhollows.com>
2024-11-27 16:25:36 +1300

    fix: load all relationships when deciding next location, including clues

--- internal/services/gameplay_service.go
+++ internal/services/gameplay_service.go
@@ -144,12 +144,10 @@ func (s *gameplayService) SuggestNextLocations(ctx context.Context, team *models
 		return nil, fmt.Errorf("determining next locations: %w", err)
 	}
 
-	if team.Instance.Settings.NavigationMethod == models.ShowMap || team.Instance.Settings.NavigationMethod == models.ShowMapAndNames {
-		for i, _ := range locations {
-			err := s.LocationService.LoadRelations(ctx, &locations[i])
-			if err != nil {
-				return nil, fmt.Errorf("loading relations for location: %w", err)
-			}
+	for i, _ := range locations {
+		err := s.LocationService.LoadRelations(ctx, &locations[i])
+		if err != nil {
+			return nil, fmt.Errorf("loading relations for location: %w", err)
 		}
 	}
 
