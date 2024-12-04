aa78f222a74f7e6593d964f4d8a77a9d290aa1c1
nathanhollows <me@nathanhollows.com>
2024-12-04 23:28:22 +1300

    feat: collapsible blocks, autoclose if the list is too long, auto open new blocks

--- internal/handlers/admin/blocks.go
+++ internal/handlers/admin/blocks.go
@@ -103,7 +103,7 @@ func (h *AdminHandler) BlockNewPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = templates.RenderAdminBlock(user.CurrentInstance.Settings, block).Render(r.Context(), w)
+	err = templates.RenderAdminBlock(user.CurrentInstance.Settings, block, true).Render(r.Context(), w)
 	if err != nil {
 		h.Logger.Error("BlockNewPost: rendering template", "error", err)
 	}
