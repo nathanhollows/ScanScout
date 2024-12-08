db32ee38aec5c02cbfbcd7012fed5d6d3966c963
nathanhollows <me@nathanhollows.com>
2024-12-08 20:32:30 +1300

    test: use new methods/dep. req's for services

--- internal/services/location_service_test.go
+++ internal/services/location_service_test.go
@@ -1,50 +1,32 @@
 package services_test
 
 import (
-	"context"
+	"log/slog"
+	"os"
 	"testing"
 
+	"github.com/nathanhollows/Rapua/db"
+	"github.com/nathanhollows/Rapua/internal/migrate"
 	"github.com/nathanhollows/Rapua/internal/services"
-	"github.com/nathanhollows/Rapua/models"
-	"github.com/stretchr/testify/assert"
 	"github.com/nathanhollows/Rapua/repositories"
+	"github.com/uptrace/bun"
 )
 
-func setupLocationService(t *testing.T) (services.LocationService, func()) {
-	cleanup := models.SetupTestDB(t)
-	clueRepo := repositories.NewClueRepository()
-	locationService := services.NewLocationService(clueRepo)
-	return locationService, cleanup
-}
+func setupLocationService(t *testing.T) (services.LocationService, *bun.DB) {
+	os.Setenv("DB_CONNECTION", "file::memory:?cache=shared")
+	os.Setenv("DB_TYPE", "sqlite3")
+	db := db.MustOpen()
+	defer db.Close()
 
-func TestLocationService_LoadCluesForLocation(t *testing.T) {
-	locationService, cleanup := setupLocationService(t)
-	defer cleanup()
+	// Create tables
+	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+	migrate.CreateTables(logger, db)
 
-	ctx := context.Background()
-	locationID := "location-1"
-
-	// Save some clues for testing
-	clueRepo := repositories.NewClueRepository()
-	clue1 := &models.Clue{
-		InstanceID: "instance-1",
-		LocationID: locationID,
-		Content:    "Clue 1",
-	}
-	clue2 := &models.Clue{
-		InstanceID: "instance-2",
-		LocationID: locationID,
-		Content:    "Clue 2",
-	}
-	err := clueRepo.Save(ctx, clue1)
-	assert.NoError(t, err, "expected no error when saving clue 1")
-	err = clueRepo.Save(ctx, clue2)
-	assert.NoError(t, err, "expected no error when saving clue 2")
-
-	// Create location and load clues
-	location := &models.Location{ID: locationID}
-	err = locationService.LoadCluesForLocation(ctx, location)
-
-	assert.NoError(t, err, "expected no error when loading clues for location")
-	assert.Len(t, location.Clues, 2, "expected two clues to be loaded")
+	clueRepo := repositories.NewClueRepository(db)
+	locationRepo := repositories.NewLocationRepository(db)
+	markerRepo := repositories.NewMarkerRepository(db)
+	blockStateRepo := repositories.NewBlockStateRepository(db)
+	blockRepo := repositories.NewBlockRepository(db, blockStateRepo)
+	locationService := services.NewLocationService(clueRepo, locationRepo, markerRepo, blockRepo)
+	return locationService, db
 }
