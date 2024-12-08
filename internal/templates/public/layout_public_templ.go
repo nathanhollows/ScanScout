d3bc2961d0ae4db718b608eeb2431bdeb75fafb0
nathanhollows <me@nathanhollows.com>
2024-12-08 20:35:35 +1300

    fix: include hyperscript in public fixes a bug in admin
    The only <script> w/ src tags that are run with htmx are those loaded in
    the first instance. Since / can link to /admin it means some sources
    don't load

--- internal/templates/public/layout_public_templ.go
+++ internal/templates/public/layout_public_templ.go
@@ -122,7 +122,7 @@ func publicFooter() templ.Component {
 		var templ_7745c5c3_Var5 string
 		templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(currYear())
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/public/layout_public.templ`, Line: 88, Col: 30}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/public/layout_public.templ`, Line: 89, Col: 30}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
 		if templ_7745c5c3_Err != nil {
