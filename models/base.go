ad5efaf226c2f11f2aaf3a4a63d4570b084dbf6a
nathanhollows <me@nathanhollows.com>
2024-12-15 23:53:02 +1300

    refactor: no more deleted_at in models, hard delete instead

--- models/base.go
+++ models/base.go
@@ -7,5 +7,4 @@ import (
 type baseModel struct {
 	CreatedAt time.Time `bun:"created_at,nullzero,notnull,default:current_timestamp"`
 	UpdatedAt time.Time `bun:"updated_at,nullzero,notnull,default:current_timestamp"`
-	DeletedAt time.Time `bun:"deleted_at,soft_delete,nullzero"`
 }
