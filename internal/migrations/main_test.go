531c3d0abac4d77d36376d4fab959621b164ee20
nathanhollows <me@nathanhollows.com>
2024-12-15 22:21:59 +1300

    test(migrations): full migrations up and down, to ensure each step runs
    NOTE: This does NOT mean the migrations run as expected, only that they
    run. Behaviour should be be tested through the repos and services.
    
    For example, this runs on an empty in-memory database. It doesn't check
    whether or not old data is successfully updated, for instance.

---
+++ internal/migrations/main_test.go
@@ -0,0 +1,47 @@
+package migrations_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/nathanhollows/Rapua/db"
+	"github.com/nathanhollows/Rapua/internal/migrations"
+	"github.com/stretchr/testify/assert"
+	"github.com/uptrace/bun/migrate"
+)
+
+// TestFullMigration ensures the full suite runs up and with without error.
+// This only tests the migrations, not the repository or service.
+// Repository and service tests should ensure the migrations are correct.
+func TestFullMigration(t *testing.T) {
+	os.Setenv("DB_CONNECTION", "file::memory:?cache=shared")
+	os.Setenv("DB_TYPE", "sqlite3")
+	db := db.MustOpen()
+	ctx := context.Background()
+
+	// Setup the migrator
+	migrator := migrate.NewMigrator(db, migrations.Migrations)
+	if err := migrator.Init(ctx); err != nil {
+		assert.NoError(t, err)
+	}
+
+	if err := migrator.Lock(ctx); err != nil {
+		assert.NoError(t, err)
+	}
+	defer migrator.Unlock(ctx)
+
+	// Migrate up
+	_, err := migrator.Migrate(ctx)
+	if err != nil {
+		assert.NoError(t, err)
+	}
+
+	// Rollback the migrations
+	_, err = migrator.Rollback(ctx)
+	if err != nil {
+		assert.NoError(t, err)
+	}
+
+	db.Close()
+}
