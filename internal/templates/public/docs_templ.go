dd3ce3b84db5cb0d730117779a954f63a9ce4927
nathanhollows <me@nathanhollows.com>
2024-12-17 23:48:41 +1300

    style: shrinking sub menu titles for consistency

--- internal/templates/public/docs_templ.go
+++ internal/templates/public/docs_templ.go
@@ -105,7 +105,7 @@ func recursiveSidebar(topPage *services.DocPage, page *services.DocPage, sidebar
 					var templ_7745c5c3_Var4 string
 					templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(page.Title)
 					if templ_7745c5c3_Err != nil {
-						return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/public/docs.templ`, Line: 37, Col: 73}
+						return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/public/docs.templ`, Line: 37, Col: 78}
 					}
 					_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 					if templ_7745c5c3_Err != nil {
