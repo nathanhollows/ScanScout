79b01367f39031b3c4568d7b3f2ef106ea540258
nathanhollows <me@nathanhollows.com>
2025-02-20 16:31:58 +1300

    docs: error comment

--- internal/services/errors.go
+++ internal/services/errors.go
@@ -11,6 +11,7 @@ var (
 
 var ErrInvalidArgument = errors.New("invalid argument")
 
+// NewValidationError returns a new error indicating that a parameter is invalid.
 func NewValidationError(param string) error {
 	return fmt.Errorf("%w: %s cannot be empty", ErrInvalidArgument, param)
 }
